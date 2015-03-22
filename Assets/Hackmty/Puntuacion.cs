0fae6eab70d8b7957997147cd000853ddd1e797b
robcastor8 <robcastor8@gmail.com>
2015-03-22 12:27:56 -0600

    SI

---
+++ Assets/Hackmty/Puntuacion.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Puntuacion : MonoBehaviour {
+
+	private Text _puntuacion;
+	private Arabes _controlador;
+
+	// Use this for initialization
+	void Start () {
+		_controlador = FindObjectOfType<Arabes>();
+		_puntuacion = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_puntuacion.text = _controlador.Puntuacion.ToString("0.00");
+	}
+}
